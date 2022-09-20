fbaf52947ccca462fb81c5e6e3e064152277f513
Mohammod Ali <mohammodali651@gmail.com>
2022-09-21 04:39:30 +0600

    make card and show flag

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import Country from '../Country/Country';
+import React, { useEffect, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
-    const [countries, setCountries] = useState([])
+  const [countries, setCountries] = useState([]);
 
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-    },[])
-
-    return (
-        <div>
-            <h1>Hello from Countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country name={country.name.common} ></Country>)
-            }
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello from Countries: {countries.length}</h1>
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country 
+          country={country}
+          key={country.cca3}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
